a28b9ebc6d366be4da303f1271c78b1258c82911
ml <mliu78@jh.edu>
2022-11-14 14:47:09 -0500

    defined to, from, and compare functions

--- src/otter.mli
+++ src/otter.mli
@@ -2,20 +2,23 @@ module Function = struct
  type params = (string * Type.t) list
  type ret = Type.t
  type name = string
- let compare = 
- let sanitize = 
+ let compare (func1:Function) (func2:Function) = int
+ let to_func (func: string) = Function
+ let from_func (func: Function) = string
 end
 
 module Module = struct
  type name = string
  type params = (string * Type.t) list
  let compare = 
- let sanitize = 
+ let to_module (func: string) = Function
+ let from_module (func: Function) = string
 end
 
 module Variable = struct
  type name = string
  type t = Type.t
  let compare = 
- let sanitize = 
+ let to_variable (func: string) = Function
+ let from_variable (func: Function) = string
 end
