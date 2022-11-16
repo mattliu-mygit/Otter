69ad0a40be5dcdf0836a2b6b278b1e28e63cd29e
ml <mliu78@jh.edu>
2022-11-16 12:33:28 -0500

    updates

--- src/otter_lib.mli
+++ src/otter_lib.mli
@@ -3,32 +3,39 @@
 module Function = struct
  type params = (string * Type.t) list
  type ret = Type.t
- type name = string
+ let name = string
  let compare (func1:Function) (func2:Function) = int
  let to_func (func: string) = Function
  let from_func (func: Function) = string
 end
 
 module Module = struct
- type name = string
  type params = (string * Type.t) list
+ let name = string
  let compare (mod1: Variable) (mod2:Variable) = int
  let to_module (func: string) = Function
  let from_module (func: Function) = string
 end
 
 module Variable = struct
- type name = string
  type t = Type.t
+ let name = string
  let compare (var1: Variable) (var2:Variable) = int
  let to_variable (func: string) = Function
  let from_variable (func: Function) = string
 end
 
 module Undetermined = struct
- type name = string
+ type params = (string * Type.t) list
  type t = Type.t
+ let name = string
  let compare = int
  let to_undetermined (func: string) = Function
  let from_undetermined (func: Function) = string
 end
+
+let sanitize (file: string list) = string list
+
+let check_indent (file: string list) = bool
+
+let check_parameter_count (file: string list) = bool
