3382f57a238e5ae3e155196f772d58dda7b78212
ml <mliu78@jh.edu>
2022-12-06 16:02:51 -0500

    fixed getComments

--- lib/otter_lib.ml
+++ lib/otter_lib.ml
@@ -8,12 +8,12 @@ open Str;;
 *)
 module type Comment = sig
   type t
-  val make : string list -> t
+  val make : string -> t
 end
 
 module type Unknown = sig
   type t
-  val make : string list -> t
+  val make : string -> t
 end
 
 module type Function = sig
@@ -23,16 +23,16 @@ end
 
 module type Block = sig
   type t
-  val make : string list -> t
+  val make : string -> t
 end
 
 module Comment = struct
-  type t = {content: string list}
+  type t = {content: string}
   let make s = {content = s}
 end
 
 module Unknown = struct
-  type t = {content: string list}
+  type t = {content: string}
   let make s = {content = s}
 end
 
@@ -72,11 +72,19 @@ let start_comment (line: string) =
  Str.string_match comment_regexp line 0
 ;;
 
-let rec get_comment (str:string) (num_open: int): (Block.t*string list) = 
+let rec get_comment (str:string) (num_open: int) (acc:string): (Block.t*string) = 
  match num_open with
-    | 0 -> (Block.make total, substrs)
+    | 0 -> (Block.make acc, str)
     | _ -> 
-
+     let first_close = Str.search_forward end_comment_regexp str 0 in
+     (match Str.string_match comment_regexp str 0 with
+      | true -> 
+       let second_open = Str.search_forward comment_regexp str 0 in
+       (match second_open < first_close with
+        | true -> get_comment (Str.string_after str (second_open+2)) (num_open + 1) (acc^(Str.string_before str (second_open+2)))
+        | false -> (Block.make (acc^(Str.string_before str (first_close+2))), (Str.string_after str (first_close+2))))
+      | false -> 
+       get_comment (Str.string_after str (first_close+2)) (num_open - 1) (acc^(Str.string_before str (first_close+2))))
 
 
     (* match num_open with
