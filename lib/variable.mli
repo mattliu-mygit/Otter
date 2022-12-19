8628dc3028ec8a829e0283ea076e65c6c461593b
kyang117 <kyang11702@gmail.com>
2022-12-19 13:15:25 -0500

    added return_type field in variable record and get_name_and_return_type function

--- lib/variable.mli
+++ lib/variable.mli
@@ -2,6 +2,7 @@ type variable =
 {
   name: string;
   content: string;
+  return_type: string;
 }
 
 val variable_regexp: Str.regexp
@@ -9,5 +10,7 @@ val variable_regexp: Str.regexp
 val end_variable_regexp: Str.regexp
 
 val start_variable: string -> bool
+
+val get_name_and_return_type: string -> (string * string)
 
 val get_variable: string -> (variable * string)
